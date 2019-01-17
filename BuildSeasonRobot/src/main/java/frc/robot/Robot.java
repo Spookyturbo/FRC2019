48f594d7494339116aeef33b9f579b5cac0e99e2
Nolan <nolan.dwenger@reyn.org>
2019-01-17 16:31:19 -0500

    Added needed comment for sure.

--- BuildSeasonRobot/src/main/java/frc/robot/Robot.java
+++ BuildSeasonRobot/src/main/java/frc/robot/Robot.java
@@ -22,7 +22,7 @@ public class Robot extends TimedRobot {
   WPI_VictorSPX rightMotor = new WPI_VictorSPX(3);
   Joystick Joy = new Joystick(0);
 double Angle = 0;
-//Heya its me sans undertale
+//Donde esta la biblioteca?
 MecanumDrive drive = new MecanumDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor)
 
   private static final String kDefaultAuto = "Default";
