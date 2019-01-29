e34a740cd56c93d29b4e0c91336fe7ce7867438e
Nate <natehryan@gmail.com>
2019-01-29 16:54:55 -0500

    f

--- BuildSeasonRobot/src/main/java/frc/robot/Robot.java
+++ BuildSeasonRobot/src/main/java/frc/robot/Robot.java
@@ -22,7 +22,7 @@ import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-import com.kauailabs.navx.frc.AHRS;;
+import com.kauailabs.navx.frc.AHRS;
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
