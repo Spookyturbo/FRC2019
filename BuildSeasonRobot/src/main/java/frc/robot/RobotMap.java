5ad0ffae741da17f7306a3b508ea3cfe2a60ea6c
Nolan <nolan.dwenger@reyn.org>
2019-02-02 15:33:35 -0500

    Added constants for Arm

--- BuildSeasonRobot/src/main/java/frc/robot/RobotMap.java
+++ BuildSeasonRobot/src/main/java/frc/robot/RobotMap.java
@@ -11,8 +11,9 @@ public class RobotMap {
         public final static int BLDrive = 7;
         public final static int FRDrive = 6;
         public final static int BRDrive = 12;
-  
+        public final static int armMotor = 9;
         public final static int intake = 14;
     }
-
+public final static int armlimitLower = 3
+public final static int armlimitUpper = 13
 }   
