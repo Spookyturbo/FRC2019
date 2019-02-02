aa9719d6c39beda4e50758f86aa818f5e86998a8
Nate <natehryan@gmail.com>
2019-02-02 11:02:16 -0500

    d

--- BuildSeasonRobot/src/main/java/frc/component/Jacks.java
+++ BuildSeasonRobot/src/main/java/frc/component/Jacks.java
@@ -8,25 +8,35 @@
 package frc.component;
 
 import frc.util.Component;
-
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 /**
  * Add your docs here.
  */
 public class Jacks implements Component{
+
+    WPI_VictorSPX J1 = new WPI_VictorSPX(RobotMap.Motors.FRDrive);
+    WPI_VictorSPX J2 = new WPI_VictorSPX(RobotMap.Motors.FRDrive);
 
 
     //Store a static instance and create it for the singleton pattern
     private static Jacks instance = new Jacks();
 
     private Jacks() {
+
+
         //Just here to remove the public constructor
     }
 
     @Override
     public void execute() {
+
+
         //Code ran every loop
     }
     public static Jacks getInstance() {
+
+
+
       return instance;
   }
 
