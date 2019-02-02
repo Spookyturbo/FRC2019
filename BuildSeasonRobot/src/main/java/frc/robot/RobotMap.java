9f31af70bb744fa0b023a035bcd6b1017ed32da2
Nate <natehryan@gmail.com>
2019-02-02 13:00:47 -0500

    f

--- BuildSeasonRobot/src/main/java/frc/robot/RobotMap.java
+++ BuildSeasonRobot/src/main/java/frc/robot/RobotMap.java
@@ -2,6 +2,8 @@ package frc.robot;
 
 public class RobotMap {
 
+    public final static int upperLimitSwitch = 0;
+    public final static int lowerLimitSwitch = 5;
     public final static class Motors {
         public final static int J1Drive = 1;
         public final static int J2Drive = 2;
@@ -9,7 +11,7 @@ public class RobotMap {
         public final static int BLDrive = 7;
         public final static int FRDrive = 6;
         public final static int BRDrive = 12;
-
+  
         public final static int intake = 14;
     }
 
