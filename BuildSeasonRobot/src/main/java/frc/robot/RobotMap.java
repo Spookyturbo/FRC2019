4a0d0ce30204dedcb1f05dec16735b16d31e8fd7
Byers <saturnfictional@gmail.com>
2019-02-02 15:54:43 -0500

    hi :)  wrist is done

--- BuildSeasonRobot/src/main/java/frc/robot/RobotMap.java
+++ BuildSeasonRobot/src/main/java/frc/robot/RobotMap.java
@@ -4,6 +4,9 @@ public class RobotMap {
 
     public final static int upperLimitSwitch = 0;
     public final static int lowerLimitSwitch = 5;
+    public final static int wLowerLimitSwitch = 2;
+    public final static int wUpperLimitSwitch = 1; 
+
     public final static class Motors {
         public final static int J1Drive = 1;
         public final static int J2Drive = 2;
@@ -12,6 +15,7 @@ public class RobotMap {
         public final static int FRDrive = 6;
         public final static int BRDrive = 12;
         public final static int armMotor = 9;
+        public final static int wrist = 2; 
         public final static int intake = 14;
     }
 public final static int armlimitLower = 3;
