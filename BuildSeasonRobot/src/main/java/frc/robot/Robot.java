5d5bab0e280a221101a3f1b233c8b91aa32be3e3
Andrew <bailey.andrew202@gmail.com>
2019-02-02 13:39:28 -0500

    Arm/Camera

--- BuildSeasonRobot/src/main/java/frc/robot/Robot.java
+++ BuildSeasonRobot/src/main/java/frc/robot/Robot.java
@@ -8,31 +8,28 @@ import frc.util.Component;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SPI;
 
 import java.util.ArrayList;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
 import com.kauailabs.navx.frc.AHRS;
+
+
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
 
 public class Robot extends TimedRobot {
   ArrayList<Component> components = new ArrayList<>();
   //Xbox Control
   XboxController xbox = new XboxController(OI.Driver.port);
-
-  WPI_VictorSPX FL = new WPI_VictorSPX(RobotMap.Motors.FLDrive);
-  WPI_VictorSPX BL = new WPI_VictorSPX(RobotMap.Motors.BLDrive);
-  WPI_VictorSPX FR = new WPI_VictorSPX(RobotMap.Motors.FRDrive);
-  WPI_VictorSPX BR = new WPI_VictorSPX(RobotMap.Motors.BRDrive);
-
-  //WPI_VictorSPX intake = new WPI_VictorSPX(RobotMap.Motors.intake);
-
-  MecanumDrive drive = new MecanumDrive(FL, BL, FR, BR);
-
   AHRS gyro;
 
   Encoder leftEncoder = new Encoder(0, 1);
@@ -40,21 +37,20 @@ public class Robot extends TimedRobot {
 
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
-
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
    */
   @Override
   public void robotInit() {
-    components.add(Jacks.getInstance());
-    SmartDashboard.putData("Auto choices", m_chooser);
 
-    leftEncoder.setName("Encoders", "Left");
-    rightEncoder.setName("Encoders", "Right");
 
-    LiveWindow.add(leftEncoder);
-    LiveWindow.add(rightEncoder);
+    //If the gyro is not plugged in this can throw an error, make sure it doesn't crash the robot
+    try {
+      gyro = new AHRS(SPI.Port.kMXP);
+    } catch (RuntimeException e) { 
+      DriverStation.reportError("Error instantiating navX MXP:  " + e.getMessage(), true); 
+    }
   }
 
   /**
@@ -88,34 +84,28 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    //gyro.reset();
   }
 
   /**
    * This function is called periodically during operator control.
    */
+  
   @Override
   public void teleopPeriodic() {
-    double y = -xbox.getY(Hand.kLeft);
     double x = xbox.getX(Hand.kLeft);
+    double y = xbox.getY(Hand.kLeft);
     double rotate = xbox.getX(Hand.kRight);
 
     drive.driveCartesian(x, y, rotate);
     
-    // if(xbox.getBumper(Hand.kLeft)) {
-    //   intake.set(1f);
-    // }
-    // else if(xbox.getBumper(Hand.kRight)) {
-    //   intake.set(-1f);
-    // }
-    // else {
-    //   intake.set(0);
-    // }
     //run all of our components
     for(Component component : components) {
       component.execute();
     }
   }
+    
+
+
 
   /**
    * This function is called periodically during test mode.
