1f503e4a75a4fcf5ab30a16fa02cd3308c97f95a
Andy Riedlinger <technicalandyr@gmail.com>
2019-02-15 15:39:46 -0500

    m

--- BuildSeasonRobot/src/main/java/frc/procedure/GyroTurning.java
+++ BuildSeasonRobot/src/main/java/frc/procedure/GyroTurning.java
@@ -7,8 +7,45 @@
 
 package frc.procedure;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /**
  * Add your docs here.
  */
 public class GyroTurning {
+
+    public void run() {
+        turnController.setP(SmartDashboard.getNumber("TurnP", turnController.getP()));
+        turnController.setI(SmartDashboard.getNumber("TurnI", turnController.getI()));
+        turnController.setD(SmartDashboard.getNumber("TurnD", turnController.getD()));
+
+        System.out.println("Error: " + turnController.getError());
+
+        double Angle = gyro.getAngle() % 360;
+        SmartDashboard.putNumber("Angle", gyro.getAngle());
+        double MotorSpeed = SmartDashboard.getNumber("MotorSpeed", drive.FL.get());
+        System.out.println(MotorSpeed);
+        SmartDashboard.putNumber("MotorSpeed", MotorSpeed);
+        if (OI.ControlProfile.driver.getYButtonReleased()) {
+            turnController.setSetpoint(0);
+        }
+        if (OI.ControlProfile.driver.getBButtonReleased()) {
+            turnController.setSetpoint(90);
+        }
+        if (OI.ControlProfile.driver.getAButtonReleased()) {
+            turnController.setSetpoint(180);
+        }
+        if (OI.ControlProfile.driver.getXButtonReleased()) {
+            turnController.setSetpoint(-90);
+        }
+        System.out.println("SetpointL " + turnController.getSetpoint());
+        System.out.println("turn rate: " + turnRate);
+        drive.driveCartesian(0, 0, turnRate);
+    }
+
+    //Scale the angle input between -180 and 180
+    public void setAngle(double angle) {
+        turnController.setSetpoint(angle);
+    }
+
 }
